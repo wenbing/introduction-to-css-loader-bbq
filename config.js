88917b3d857c0d97226c119b9373a578001d949e
zhengwenbing <zhengwenbing@meituan.com>
2016-07-17 22:34:29 +0800

    add tests

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV === undefined) {
+  process.env.NODE_ENV = 'development';
+}
 var basedir = __dirname;
 var outputdir = __dirname + '/public';
 var publicPath = '/';
--- test/client.js
+++ test/client.js
@@ -1,5 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
 const tape = require('tape');
+
+const config = require('../config');
 
 tape(42, (t) => {
   t.end();
 });
+
+tape('public', t => {
+  t.plan(2)
+
+  const red = 'color: red;';
+  const contents = fs.readFileSync(path.resolve(__dirname, '../public/client.bundle.js'), { encoding: 'utf8' });
+  t.ok(contents.indexOf(red) !== -1, 'public contents is ok');
+  t.ok(require('../lib/client.css.js').__cssText__.indexOf(red) !== -1, 'lib cssText is ok');
+});
+
+
